2cc7a267812a5db5fd643fab5e7454d113757a02
John Jennings <jej2879@cs.utexas.edu>
2024-04-26 02:17:11 -0500

    Added white-box testing code for Fourier predictor

--- fourier-test/double-fourier-test.cc
+++ fourier-test/double-fourier-test.cc
@@ -12,7 +12,8 @@ const int WAVE_COUNT = 1 << WAVE_BITS;
 const int LINE_COUNT = 1 << LINE_BITS;
 
 typedef struct Wave {
-	double amplitude;
+	double cos_amplitude;
+	double sin_amplitude;
 	uint8_t length;
 	uint8_t counter;
 	bool valid;
@@ -35,7 +36,8 @@ void initialize_branch_predictor() {
 		::Line* line = &::bank[i];
 		for (int j = 0; j < ::WAVE_COUNT; j++) {
 			::Wave* wave = &(line->waves[j]);
-			wave->amplitude = 0;
+			wave->cos_amplitude = 0;
+			wave->sin_amplitude = 0;
 			wave->length = j + 1;
 			wave->counter = 0;
 			wave->valid = false;
@@ -49,7 +51,8 @@ uint8_t predict_branch(uint64_t ip) {
 		line->ip_tag = ip;
 		for (int j = 0; j < ::WAVE_COUNT; j++) {
                         ::Wave* wave = &(line->waves[j]);
-                        wave->amplitude = 0;
+                        wave->cos_amplitude = 0;
+			wave->sin_amplitude = 0;
                         wave->length = j + 1;
                         wave->counter = 0;
                         wave->valid = false;
@@ -59,9 +62,10 @@ uint8_t predict_branch(uint64_t ip) {
 	double branch_value = 0;
 	for (int j = 0; j < ::WAVE_COUNT; j++) {
 		::Wave* wave = &(line->waves[j]);
-		branch_value += wave->valid * cos(2.0 * M_PI * wave->counter / wave->length) * wave->amplitude;
+		branch_value += wave->valid * cos(2.0 * M_PI * wave->counter / wave->length) * wave->cos_amplitude;
+		branch_value += wave->valid * sin(2.0 * M_PI * wave->counter / wave->length) * wave->sin_amplitude;
 	}
-	printf("Branch value: %f; ", branch_value);
+	printf("Branch value: %11f; ", branch_value);
 	return (branch_value > 0);
 }
 
@@ -70,7 +74,8 @@ void last_branch_result(uint64_t ip, uint64_t branch_target, uint8_t taken, uint
 	double branch_value = taken ? 1 : -1;
 	for (int j = 0; j < WAVE_COUNT; j++) {
 		::Wave* wave = &(line->waves[j]);
-		wave->amplitude += branch_value * cos(2.0 * M_PI * wave->counter / wave->length);
+		wave->cos_amplitude += branch_value * cos(2.0 * M_PI * wave->counter / wave->length);
+		wave->sin_amplitude += branch_value * sin(2.0 * M_PI * wave->counter / wave->length);
 		wave->counter = (wave->counter == 0) ? (wave->length - 1) : (wave->counter - 1);
 		if (wave->counter == 0) {
 			wave->valid = true;
@@ -83,7 +88,7 @@ int main() {
 	initialize_branch_predictor();
 	for (int i = 0; i < 1000; i++) {
 		bool prediction = predict_branch(0);
-		bool branch = (0b1101 >> (i % 4)) & 1;
+		bool branch = (0b01101 >> (i % 5)) & 1;
 		last_branch_result(0, 0, branch, 0);
 		if (prediction == branch) {
 			c++;
@@ -94,7 +99,7 @@ int main() {
 	::Line* line = &::bank[0];
 	for (int j = 0; j < WAVE_COUNT; j++) {
 		::Wave* wave = &(line->waves[j]);
-		printf("Amplitude at wavelength %d: %f\n", wave->length, wave->amplitude);
+		printf("Amplitude at wavelength %2d: (cos: %11f, sin: %11f)\n", wave->length, wave->cos_amplitude, wave->sin_amplitude);
 	}
 	return 0;
 }
