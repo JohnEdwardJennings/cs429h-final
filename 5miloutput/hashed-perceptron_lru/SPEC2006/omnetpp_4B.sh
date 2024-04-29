e80b0214884e5a9745e80422f76a63dd8a96b085
JohnEdwardJennings <145416575+JohnEdwardJennings@users.noreply.github.com>
2024-04-29 17:37:54 -0500

    Removed buggy hashed-perceptron output

--- 5miloutput/hashed-perceptron_lru/SPEC2006/omnetpp_4B.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-stdbuf --output=L /u/sahilsc/destination_directory/ChampSim/bin/hashed-perceptron_lru_1_core_champsim \
-    --warmup-instructions 5000000 \
-    --simulation-instructions 5000000 \
-    --json /u/sahilsc/destination_directory/ChampSim/output/hashed-perceptron_lru/SPEC2006/omnetpp_4B.json \
-    /scratch/cluster/hill/traces/spec2006/omnetpp_4B.trace.xz \
-    | tee /u/sahilsc/destination_directory/ChampSim/output/hashed-perceptron_lru/SPEC2006/omnetpp_4B.txt
-
